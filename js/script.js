ec142f641118f504a89e21d2278eddd1c23034dd
Fajri <spag.959@gmail.com>
2023-12-26 17:58:29 +0700

    form validation

--- js/script.js
+++ js/script.js
@@ -30,3 +30,30 @@ form.addEventListener("submit", function (event) {
      event.preventDefault();
      sendEmail();
 });
+
+document.getElementById("formEmail").addEventListener("input", function () {
+     var form = document.getElementById("formEmail");
+     var name = form.querySelector("#senderName").value;
+     var email = form.querySelector("#senderEmail").value;
+     var title = form.querySelector("#titleMsg").value;
+     var message = form.querySelector("#msg").value;
+     var submitButton = document.getElementById("submitButton");
+     var completionMessage = document.getElementById("completionMessage");
+
+     if (
+          name.trim() !== "" &&
+          message.trim() !== "" &&
+          email.trim() !== "" &&
+          title.trim() !== "" &&
+          email.includes("@") &&
+          email.includes(".") &&
+          email.length > 5 &&
+          email.indexOf("@") < email.indexOf(".")
+     ) {
+          submitButton.removeAttribute("disabled");
+          completionMessage.style.display = "none";
+     } else {
+          submitButton.setAttribute("disabled", "disabled");
+          completionMessage.style.display = "block";
+     }
+});
